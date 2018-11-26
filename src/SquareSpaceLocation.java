00a1b6f71c6ab3f3998eed6369e7f951802cb3e3
Princess Ruthie <missruthie@gmail.com>
2018-11-26 18:33:18 -0500

    feat: Add basic squarespace location model

---
+++ src/SquareSpaceLocation.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author fieldsruthie
+ *
+ */
+public class SquareSpaceLocation {
+	private String name;
+	private double lat;
+	private double lon;
+	private String address;
+	
+	public SquareSpaceLocation(String name, double lat, double lon, String address) {	
+		this.name = name;
+		this.lat = lat;
+		this.lon = lon;
+		this.address = address;		
+	}
+
+}
