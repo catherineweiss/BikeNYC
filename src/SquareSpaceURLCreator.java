ccc549c8adc7797a4de62b35dcd0bc2bfbd54d21
Princess Ruthie <missruthie@gmail.com>
2018-11-26 18:32:20 -0500

    feat: Add square space url creator

---
+++ src/SquareSpaceURLCreator.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @author fieldsruthie
+ *
+ */
+public class SquareSpaceURLCreator {
+
+	/**
+	 * 
+	 * 
+	 * @return URL for squarespace API
+	 */
+
+	public static final String FOUR_SQUARE_VERSION = "20181101";
+	public static String createURL(double lat, double lon, String query) {
+		
+	    int limit = 10;
+		String path = "https://api.foursquare.com/v2/venues/explore/";
+		
+		String url = path + "?";
+		url += "client_id=" + PrivateVariables.FOUR_SQUARE_CLIENT_ID;
+		url += "&client_secret=" + PrivateVariables.FOUR_SQUARE_CLIENT_SECRET;
+		url += "&v=" + FOUR_SQUARE_VERSION;
+		url += "&ll=" + lat + "," + lon;
+		url += "&query=" +  query;
+		url += "limit=" + "1";
+		System.out.println(url);
+		APICaller caller = new APICaller();
+		String response = caller.callAPI(url);
+		System.out.println(response);
+		return url;
+	}
+}
