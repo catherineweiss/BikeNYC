02510a6d6282aa3d0f3447f55a7d1a0e537fa98d
catherineweiss <cweisspa@gmail.com>
2018-11-28 16:39:07 -0500

    Created main package for java files

---
+++ src/main/ProgramStarter.java
@@ -0,0 +1,24 @@
+package main;
+
+public class ProgramStarter {
+
+	public static void main(String[] args) {
+		
+//		String searchAddress = "80 Henry Street, Brooklyn";
+		String searchAddress = "20 W 34th St, New York, NY 10001";
+		GoogleURLCreator gc = new GoogleURLCreator();
+		String googleURL = gc.createURL(searchAddress);
+		APICaller ac = new APICaller();
+		String gResponse = ac.callAPI(googleURL);
+		GeocodingParser gp = new GeocodingParser();
+		gp.parseGeocodingAPIResponse(gResponse);
+		
+		System.out.println("Output from Geocoding API is an instance of Location class:");
+		System.out.println("   Name: "+ gp.getOriginLocation().getName());
+		System.out.println("   Lat: "+ gp.getOriginLocation().getLatitude());
+		System.out.println("   Long: "+ gp.getOriginLocation().getLongitude());
+		System.out.println("   Address: "+ gp.getOriginLocation().getAddress());		
+		
+		
+	}
+}
