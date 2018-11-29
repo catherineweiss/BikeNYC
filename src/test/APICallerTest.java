8c8516e183864d124c474695dce89d9e18aa5ca6
catherineweiss <cweisspa@gmail.com>
2018-11-29 15:14:04 -0500

    edited expectedResponse

--- src/test/APICallerTest.java
+++ src/test/APICallerTest.java
@@ -1,6 +1,10 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,22 +27,96 @@ public class APICallerTest {
 		String goodURL = "https://maps.googleapis.com/maps/api/geocode/"
 				+"json?address=20+W+34th+St,+New+York&region=new+york&country:US"
 				+"&key=" + GoogleAPIKey.key;
-		String expectedResponse = "{\"results\":[{\"address_components\":[{\"long_name\":\"20\",\"short_name\":\"20\",\"types\":[\"street_number\"]},{\"long_name\":\"West 34th Street\",\"short_name\":\"W 34th St\",\"types\":[\"route\"]},{\"long_name\":\"Manhattan\",\"short_name\":\"Manhattan\",\"types\":[\"political\",\"sublocality\",\"sublocality_level_1\"]},{\"long_name\":\"New York\",\"short_name\":\"New York\",\"types\":[\"locality\",\"political\"]},{\"long_name\":\"New York County\",\"short_name\":\"New York County\",\"types\":[\"administrative_area_level_2\",\"political\"]},{\"long_name\":\"New York\",\"short_name\":\"NY\",\"types\":[\"administrative_area_level_1\",\"political\"]},{\"long_name\":\"United States\",\"short_name\":\"US\",\"types\":[\"country\",\"political\"]},{\"long_name\":\"10001\",\"short_name\":\"10001\",\"types\":[\"postal_code\"]}],\"formatted_address\":\"20 W 34th St, New York, NY 10001, USA\",\"geometry\":{\"location\":{\"lat\":40.7487836,\"lng\":-73.98615769999999},\"location_type\":\"ROOFTOP\",\"viewport\":{\"northeast\":{\"lat\":40.75013258029149,\"lng\":-73.9848087197085},\"southwest\":{\"lat\":40.74743461970849,\"lng\":-73.9875066802915}}},\"place_id\":\"ChIJr92XCKlZwokRIy2j_7Rb2V8\",\"plus_code\":{\"compound_code\":\"P2X7+GG New York, United States\",\"global_code\":\"87G8P2X7+GG\"},\"types\":[\"street_address\"]}],\"status\":\"OK\"}";
+
+		String expectedResponseRaw ="{\n" + 
+				"   \"results\" : [\n" + 
+				"      {\n" + 
+				"         \"address_components\" : [\n" + 
+				"            {\n" + 
+				"               \"long_name\" : \"20\",\n" + 
+				"               \"short_name\" : \"20\",\n" + 
+				"               \"types\" : [ \"street_number\" ]\n" + 
+				"            },\n" + 
+				"            {\n" + 
+				"               \"long_name\" : \"West 34th Street\",\n" + 
+				"               \"short_name\" : \"W 34th St\",\n" + 
+				"               \"types\" : [ \"route\" ]\n" + 
+				"            },\n" + 
+				"            {\n" + 
+				"               \"long_name\" : \"Manhattan\",\n" + 
+				"               \"short_name\" : \"Manhattan\",\n" + 
+				"               \"types\" : [ \"political\", \"sublocality\", \"sublocality_level_1\" ]\n" + 
+				"            },\n" + 
+				"            {\n" + 
+				"               \"long_name\" : \"New York\",\n" + 
+				"               \"short_name\" : \"New York\",\n" + 
+				"               \"types\" : [ \"locality\", \"political\" ]\n" + 
+				"            },\n" + 
+				"            {\n" + 
+				"               \"long_name\" : \"New York County\",\n" + 
+				"               \"short_name\" : \"New York County\",\n" + 
+				"               \"types\" : [ \"administrative_area_level_2\", \"political\" ]\n" + 
+				"            },\n" + 
+				"            {\n" + 
+				"               \"long_name\" : \"New York\",\n" + 
+				"               \"short_name\" : \"NY\",\n" + 
+				"               \"types\" : [ \"administrative_area_level_1\", \"political\" ]\n" + 
+				"            },\n" + 
+				"            {\n" + 
+				"               \"long_name\" : \"United States\",\n" + 
+				"               \"short_name\" : \"US\",\n" + 
+				"               \"types\" : [ \"country\", \"political\" ]\n" + 
+				"            },\n" + 
+				"            {\n" + 
+				"               \"long_name\" : \"10001\",\n" + 
+				"               \"short_name\" : \"10001\",\n" + 
+				"               \"types\" : [ \"postal_code\" ]\n" + 
+				"            }\n" + 
+				"         ],\n" + 
+				"         \"formatted_address\" : \"20 W 34th St, New York, NY 10001, USA\",\n" + 
+				"         \"geometry\" : {\n" + 
+				"            \"location\" : {\n" + 
+				"               \"lat\" : 40.7487836,\n" + 
+				"               \"lng\" : -73.98615769999999\n" + 
+				"            },\n" + 
+				"            \"location_type\" : \"ROOFTOP\",\n" + 
+				"            \"viewport\" : {\n" + 
+				"               \"northeast\" : {\n" + 
+				"                  \"lat\" : 40.75013258029149,\n" + 
+				"                  \"lng\" : -73.9848087197085\n" + 
+				"               },\n" + 
+				"               \"southwest\" : {\n" + 
+				"                  \"lat\" : 40.74743461970849,\n" + 
+				"                  \"lng\" : -73.9875066802915\n" + 
+				"               }\n" + 
+				"            }\n" + 
+				"         },\n" + 
+				"         \"place_id\" : \"ChIJr92XCKlZwokRIy2j_7Rb2V8\",\n" + 
+				"         \"plus_code\" : {\n" + 
+				"            \"compound_code\" : \"P2X7+GG New York, United States\",\n" + 
+				"            \"global_code\" : \"87G8P2X7+GG\"\n" + 
+				"         },\n" + 
+				"         \"types\" : [ \"street_address\" ]\n" + 
+				"      }\n" + 
+				"   ],\n" + 
+				"   \"status\" : \"OK\"\n" + 
+				"}\n" + 
+				""; 
+		String expectedResponse = expectedResponseRaw.replaceAll("\n", "");
+		
 		assertEquals(expectedResponse, apiCaller.callAPI(goodURL));		
 	}
 
 	
-/*	//Test Purpose: give the APICaller a bad URL and check error
-	@Test
-	void testCallerBadURL() {
-		
-		String badURL = "https://maps.apis.com/maps/api/geocode/"
-				+"json?address=20+W+34th+St,+New+York&region=new+york&country:US"
-				+"&key=" + GoogleAPIKey.key;
-		
-		assertEquals(,);
-
-	}
-*/	
-	
+//	//Test Purpose: give the APICaller a bad URL and check error
+//	@Test
+//	void testBadURL() {		
+//		String badURL = "https://maps.apis/maps/api/geocode/"
+//				+"json?address=20+W+34th+St,+New+York&region=new+york&country:US"
+//				+"&key=" + GoogleAPIKey.key;
+//		Assertions.assertThrows(ClassCastException.class, () -> {	
+//			apiCaller.callAPI(badURL);
+//		});
+//		
+//	}
 }
