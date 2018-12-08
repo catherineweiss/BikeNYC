c5d1484d4787a71ad6e29e9d8abf5ecb480f330d
Princess Ruthie <missruthie@gmail.com>
2018-12-08 14:02:28 -0500

    feat: Update readme to show how to add keys and run app

---
+++ src/util/APIKeysSample.java
@@ -0,0 +1,9 @@
+package util;
+
+public class APIKeysSample {
+
+public static final String GOOGLE_API_KEY = "";
+public static final String FOUR_SQUARE_CLIENT_ID = "";
+public static final String FOUR_SQUARE_CLIENT_SECRET = "";
+
+}
